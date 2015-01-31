94702e08ac2733ed0928207555dc59b11895b672
Andrew Haaland <dhaaland@hvc.rr.com>
2015-01-30 19:04:16 -0500

    Fixed Blinking LED
    Fixed LED Blinking assignments indentation to work properly.

--- code/BlinkingLEDAssignment1.py
+++ code/BlinkingLEDAssignment1.py
@@ -4,12 +4,12 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 def Blink():
- for i in range(0,3):
- print "blink #" + str(i+1)
- GPIO.output(17,True)
- time.sleep(1)
- GPIO.output(17,False)
- time.sleep(1)
- print "done!!"
- GPIO.cleanup()
+	for i in range(0,3):
+		print "blink #" + str(i+1)
+		GPIO.output(17,True)
+		time.sleep(1)
+		GPIO.output(17,False)
+		time.sleep(1)
+	print "done!!"
+	GPIO.cleanup()
 Blink()
